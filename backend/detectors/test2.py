e71fc60437b3ccd8cebe65b8a2d236c05bd967a0
Lang Gao <langfr.gao@gmail.com>
2019-05-30 11:44:46 -0400

    Delete test2.py

--- backend/detectors/test2.py
+++
@@ -1,8 +0,0 @@
-
-a = {"fname":"ss","lname":"dstt","email":"as@gggttt","age":"25"}
-if("fname" in a):
-    first_name = a["fname"]
-if("lname" in a):
-    last_name = a["lname"]
-name = first_name+" " + last_name
-print(int(a["age"])+1)
